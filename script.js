1d415024fa44cd63c432a053c2220b40ec8bbc8d
alicoder123411 <sameersingla240@gmail.com>
2026-01-29 15:17:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+output.innerHTML = `
+  <tr>
+    <td colspan="2">Loading...</td>
+  </tr>
+`;
+function createPromise() {
+  return new Promise((resolve) => {
+    const time = Math.random() * 2 + 1; // 1 to 3 seconds
+    setTimeout(() => {
+      resolve(time);
+    }, time * 1000);
+  });
+}
+const p1 = createPromise();
+const p2 = createPromise();
+const p3 = createPromise();
+Promise.all([p1, p2, p3]).then((results) => {
+  output.innerHTML = ""; 
+  let maxTime = 0;
+  results.forEach((time, index) => {
+    maxTime = Math.max(maxTime, time);
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>Promise ${index + 1}</td>
+      <td>${time.toFixed(0)}</td>
+    `;
+    output.appendChild(row);
+  });
+  const totalRow = document.createElement("tr");
+  totalRow.innerHTML = `
+    <td>Total</td>
+    <td>${maxTime.toFixed(3)}</td>
+  `;
+  output.appendChild(totalRow);
+});
